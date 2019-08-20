ab9d6f38a41b6416267a54a0cebac6ee4f0d5a9d
Matt Lindeboom <mattl@surveymonkey.com>
2019-08-19 17:51:42 -0700

    Check for localEnv if not process.env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const { parsed: localEnv } = require('dotenv').config()
 const config =  { // Will be available on both server and client
-  apiURL: localEnv.API_HOST,
-  clientURL: localEnv.CLIENT_HOST
+  apiURL: localEnv ? localEnv.API_HOST : process.env.API_HOST,
+  clientURL: localEnv ? localEnv.CLIENT_HOST : process.env.CLIENT_HOST
 }
+
 module.exports = {
   serverRuntimeConfig: config,
   publicRuntimeConfig: config
