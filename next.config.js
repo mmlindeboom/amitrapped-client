ff3e46356057ec34fd382822bab39f18eb657671
Matt Lindeboom <mattl@surveymonkey.com>
2019-08-19 17:45:52 -0700

    Fix host vars

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { parsed: localEnv } = require('dotenv').config()
 
 module.exports = {
   publicRuntimeConfig: { // Will be available on both server and client
-    apiURL: localEnv.API_URL,
-    clientURL: localEnv.CLIENT_URL
+    apiURL: localEnv.API_HOST,
+    clientURL: localEnv.CLIENT_HOST
   }
 }
