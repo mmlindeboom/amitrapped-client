12a500782302736d618d33e209db4c4dc484169f
Matt Lindeboom <mattl@surveymonkey.com>
2019-08-26 09:59:53 -0700

    Removing questions.js

--- pages/questions.js
+++
@@ -1,6 +0,0 @@
-import withData from '../lib/apollo'
-import QuestionList from '../components/QuestionList'
-import AppLayout from '../components/AppLayout'
-export default withData(() => {
-  return <AppLayout><QuestionList></QuestionList></AppLayout>
-})
