5ecd63abd6a2de6eeccc339c69590cdada9eb502
Matt Lindeboom <matt.lindeboom@gmail.com>
2021-02-09 16:55:28 -0800

    Full page iframe for take action

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Head from 'next/head'
-import { Container } from 'next/app'
-import Router from 'next/router'
+import React, { useState } from "react";
+import Head from "next/head";
+import { Container } from "next/app";
+import Router from "next/router";
 import {
   Dropdown,
   Grid,
@@ -9,31 +9,34 @@ import {
   Icon,
   Image,
   Menu,
-  Responsive
-} from 'semantic-ui-react'
-import { useMutation} from '@apollo/react-hooks'
-import { gql } from 'apollo-boost'
-import { logout, isAuthenticated } from '../lib/auth'
+  Responsive,
+} from "semantic-ui-react";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import { logout, isAuthenticated } from "../lib/auth";
 
 const LOGOUT_USER = gql`
   mutation logout {
     logout
   }
-`
+`;
 
 export default function({ client, page, children }) {
-  const [logOutUser] = useMutation(LOGOUT_USER)
-  const [windowWidth, setWindowWidth] = useState(0)
-  const [authenticated] = useState(isAuthenticated())
+  const [logOutUser] = useMutation(LOGOUT_USER);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [authenticated] = useState(isAuthenticated());
   const handleLogout = () => {
-    logOutUser()
-    logout(client)
-  }
+    logOutUser();
+    logout(client);
+  };
 
   const gridStyles = {
-    maxWidth: '1024px',
-    margin: Responsive.onlyTablet.minWidth <= windowWidth ? '140px auto 0 auto' : '24px auto 0 auto'
-  }
+    maxWidth: "1024px",
+    margin:
+      Responsive.onlyTablet.minWidth <= windowWidth
+        ? "140px auto 0 auto"
+        : "24px auto 0 auto",
+  };
 
   return (
     <Container>
@@ -42,39 +45,55 @@ export default function({ client, page, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600&display=swap" rel="stylesheet"></link>
-
+        <link
+          href="https://fonts.googleapis.com/css?family=Nunito:400,600&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Menu attached="top" color="darkGrey">
-        <Menu.Item onClick={() => Router.push('/')}>
+        <Menu.Item onClick={() => Router.push("/")}>
           <Header>
             <Header.Content>Your Digital Health</Header.Content>
           </Header>
         </Menu.Item>
 
-        {page && <Menu.Item active><Icon name="angle right" color='orange' style={{position: 'absolute', left: -8}}></Icon>{page}</Menu.Item>}
-        {authenticated &&
+        {page && (
+          <Menu.Item active>
+            <Icon
+              name="angle right"
+              color="orange"
+              style={{ position: "absolute", left: -8 }}
+            ></Icon>
+            {page}
+          </Menu.Item>
+        )}
+        {authenticated && (
           <Menu.Menu position="right" color="orange">
-            <Dropdown item simple icon="setting" color='orange'>
-              <Dropdown.Menu color='darkGrey'>
-                <Dropdown.Item onClick={handleLogout}><Icon name="hand victory" color="orange"></Icon>Logout</Dropdown.Item>
+            <Dropdown item simple icon="setting" color="orange">
+              <Dropdown.Menu color="darkGrey">
+                <Dropdown.Item onClick={handleLogout}>
+                  <Icon name="hand victory" color="orange"></Icon>Logout
+                </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </Menu.Menu>
-        }
+        )}
       </Menu>
 
       <Responsive
         fireOnMount
-        onUpdate={(e, {width}) => setWindowWidth(width)}
-        minWidth={Responsive.onlyTablet.minWidth}>
-        <Image size="large" src="/static/journey.png" style={{position: 'absolute', left: 0, top: 60}}></Image>
+        onUpdate={(e, { width }) => setWindowWidth(width)}
+        minWidth={Responsive.onlyTablet.minWidth}
+      >
+        <Image
+          size="large"
+          src="/static/journey.png"
+          style={{ position: "absolute", left: 0, top: 60 }}
+        ></Image>
       </Responsive>
       <Grid verticalAlign="middle" style={gridStyles}>
-        <Grid.Column>
-          {children}
-        </Grid.Column>
+        <Grid.Column>{children}</Grid.Column>
       </Grid>
     </Container>
-  )
+  );
 }
--- pages/action.js
+++ pages/action.js
@@ -1,25 +1,36 @@
-import { useState, useEffect } from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import dynamic from 'next/dynamic'
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import dynamic from "next/dynamic";
 import AppLayout from "../components/AppLayout";
-import { GET_SERVICES } from '../data/quiz'
-import Loader from '../components/utils/Loader'
+import { GET_SERVICES } from "../data/quiz";
+import Loader from "../components/utils/Loader";
 
-const ServicesBlock = dynamic(() => import('../components/blocks/ServicesBlock'), {ssr: false})
+const ServicesBlock = dynamic(
+  () => import("../components/blocks/ServicesBlock"),
+  { ssr: false }
+);
 
 const Home = ({ client }) => {
-  const {data, loading } = useQuery(GET_SERVICES)
-
-  const [services, setServices] = useState([])
-
-  useEffect(() => {
-    setServices(data.services)
-  }, [data])
-
   return (
     <AppLayout client={client} page="Take Action">
-      <Loader isLoading={loading}></Loader>
-      <ServicesBlock services={services}></ServicesBlock>
+      <div
+        style={{
+          position: "fixed",
+          top: 48,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <iframe
+          src="http://digitalbehaviorist.com/tools-for-you"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+        ></iframe>
+      </div>
     </AppLayout>
   );
 };
